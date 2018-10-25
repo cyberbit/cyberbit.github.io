729835d29bdedeed4b7696616192c51d63a7be51
D.J. Marcolesco <dj.marcolesco@gmail.com>
2018-10-25 01:03:53 -0500

    #6: More work for header

--- website/src/App.vue
+++ website/src/App.vue
@@ -2,22 +2,25 @@
   <div class="container text-light">
     <div class="columns">
       <div class="column col-mx-auto">
-        <header class="navbar fixed">
-          <section class="navbar-section">
-          </section>
-          <section class="navbar-center">
-            <a href="/"><img src="@/assets/logo1-white.svg" class="dm-logo d-block"></a>
-              <transition name="slide-fade" mode="out-in">
-                <h2 class="dm-navbar-title text-center m-0" :key="section">
-                  {{ sectionTitles[section] }}
-                </h2>
-              </transition>
-          </section>
-          <section class="navbar-section">
-            <!-- <a @click="tab = 'about'" class="btn btn-link" :class="{ active: tab === 'about' }">About</a> -->
-            <!-- <a @click="tab = 'experience'" class="btn btn-link" :class="{ active: tab === 'experience' }">Experience</a> -->
-          </section>
-        </header>
+        <transition name="slide-out">
+          <header class="navbar dm-navbar fixed" v-show="section !== null">
+            <section class="navbar-section"></section>
+            <section class="navbar-center">
+              <a href="/">
+                <img src="@/assets/logo1-white.svg" class="dm-logo d-block">
+              </a>
+              <h3 class="dm-navbar-title text-center m-0">
+                <transition name="ticker" mode="out-in">
+                  <div :key="section">{{ sectionTitles[section] }}</div>
+                </transition>
+              </h3>
+            </section>
+            <section class="navbar-section">
+              <!-- <a @click="tab = 'about'" class="btn btn-link" :class="{ active: tab === 'about' }">About</a> -->
+              <!-- <a @click="tab = 'experience'" class="btn btn-link" :class="{ active: tab === 'experience' }">Experience</a> -->
+            </section>
+          </header>
+        </transition>
       </div>
     </div>
     <div class="dm-hero text-center">
@@ -27,7 +30,7 @@
       <h3>developer, musician, human.</h3>
     </div>
     <div class="columns">
-      <div class="column col-sm-12 col-8 col-mx-auto" v-scroll-spy="{data: 'section'}">
+      <div class="column col-sm-12 col-8 col-mx-auto" v-scroll-spy="{data: 'section', allowNoActive: true}">
         <div v-if="!tab || tab === 'about'">
           <h1>About</h1>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro quaerat consequuntur minus natus asperiores, dignissimos officiis facere, ducimus rerum, deleniti hic ipsum labore voluptatibus quae! Voluptate eligendi exercitationem qui provident.</p>
@@ -86,7 +89,7 @@ export default {
   data () {
     return {
       tab: false,
-      section: 0,
+      section: null,
       sectionTitles: [
         'About',
         'Experience',
@@ -141,10 +144,11 @@ export default {
     width: 100%;
 
     background: $site-bg-color;
-    border-bottom: $light-color solid 1px;
+    // border-bottom: $primary-color solid 3px;
+    box-shadow: $site-bg-color 0 -20px 40px 40px;
 
     .dm-logo {
-      height: 4rem;
+      height: 3.5rem;
       padding: 1rem;
     }
 
@@ -161,21 +165,17 @@ export default {
       }
     }
 
-    // .dm-navbar-title {
-    //   span {
-    //     display: none;
-    //   }
-
-    //   span.dm-show {
-    //     display: inline;
-    //   }
-    // }
+    .dm-navbar-title {
+      border-left: $primary-color solid 3px;
+      width: 190px;
+    }
   }
 
   // Hero block
   .dm-hero {
     position: relative;
-    padding: 4em;
+    padding: 4em 0;
+    overflow: hidden;
 
     img {
       height: 100%;
@@ -187,7 +187,7 @@ export default {
     h1 {
       font-size: 2.5rem;
       display: inline-block;
-      border-bottom: $gray-color-dark 1px solid;
+      border-bottom: $primary-color 3px solid;
       letter-spacing: 1.5px;
     }
 
@@ -201,22 +201,45 @@ export default {
     }
   }
 
-  .dm-navbar-title {
-    width: 180px;
+  .dm-navbar {
+    overflow: hidden;
   }
 
   // Transitions
-  .slide-fade-enter-active {
-    transition: all .1s ease;
+  .ticker {
+    &-enter-active {
+      transition: all .1s ease;
+    }
+
+    &-leave-active {
+      transition: all .1s ease;
+    }
+
+    &-enter {
+      transform: translateY(20px);
+      overflow: hidden;
+      opacity: 0;
+    }
+
+    &-leave-to {
+      transform: translateY(-50px);
+      overflow: hidden;
+      opacity: 0;
+    }
   }
-  .slide-fade-leave-active {
-    transition: all .1s cubic-bezier(1.0, 0.5, 0.8, 1.0);
-  }
-  .slide-fade-enter, .slide-fade-leave-to
-  /* .slide-fade-leave-active for <2.1.8 */ {
-    transform: translateY(100px);
-    overflow: hidden;
-    opacity: 0;
+
+  .slide-out {
+    &-enter-active {
+      transition: all .5s ease;
+    }
+
+    &-leave-active {
+      transition: all .3s ease;
+    }
+
+    &-enter, &-leave-to {
+      transform: translateY(-100px);
+    }
   }
 
 </style>
